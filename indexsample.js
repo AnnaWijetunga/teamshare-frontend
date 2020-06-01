33dce468f700019cbe1d550aa7b91810b0fbadbf
AnnaWijetunga <annawijetunga@gmail.com>
2020-06-01 19:42:47 -0400

    Update new file notes

--- indexsample.js
+++ indexsample.js
@@ -1,6 +1,7 @@
 // This file is a sample/test file - to be deleted eventually
 // This was created to play a bit with React Native
 // Intro lets you get started immediately in the browser with interactive examples
+// Apparently, to really see this in a browser, this code should end up in App.js, not this new file
 
 import React from 'react';
 import { Text, View } from 'react-native';
