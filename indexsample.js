7f0ede937bbd97b669a0565b53a3eee7b4c17bb4
AnnaWijetunga <annawijetunga@gmail.com>
2020-06-01 19:38:42 -0400

    Import text and view from React Native

--- indexsample.js
+++ indexsample.js
@@ -1,3 +1,15 @@
 // This file is a sample/test file - to be deleted eventually
 // This was created to play a bit with React Native
 
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function YourApp() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>
+        Try edit me! 🎉
+      </Text>
+    </View>
+  );
+}
